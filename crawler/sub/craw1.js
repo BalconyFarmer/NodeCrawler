6ba113a8ee7c93759caf934cc5d5ac9f1a50c83e
王科威 <2@1.com>
2023-01-30 16:22:08 +0800

    pm2 服务集群

--- crawler/sub/craw1.js
+++ crawler/sub/craw1.js
@@ -26,75 +26,78 @@ async function run1(browser, content) {
 
     let indexArr = [
         0,
-        // 2,
-        // 3,
-        // 4
+        2,
+        3,
+        4
     ]
-    let item = 0
-    // indexArr.forEach(item => {
-    let type = ""
-    switch (item) {
-        case 0:
-            type = "0※ 学习专区 ※"
-            break
-        case 2:
-            type = "2※ 海水生物讨论区※"
-            break
-        case 3:
-            type = "3※ 海缸展示及研讨区※"
-            break
-        case 4:
-            type = "4※ 器材讨论区※"
-            break
-    }
-    let a = $('.fl_tb').eq(item).html()
-    let new22 = cheerio.load(a, {
-        xmlMode: true,
-        ignoreWhitespace: true,
-        lowerCaseTags: true
-    })
 
-    let _arr = []
-    new22('tr').each(async function (i, elem) {
-        let _a1 = cheerio.load($(this).html(), {
+    for (let i = 0; i < indexArr.length; i++) {
+        let item = indexArr[i]
+        let type = ""
+        switch (item) {
+            case 0:
+                type = "0※ 学习专区 ※"
+                break
+            case 2:
+                type = "2※ 海水生物讨论区※"
+                break
+            case 3:
+                type = "3※ 海缸展示及研讨区※"
+                break
+            case 4:
+                type = "4※ 器材讨论区※"
+                break
+        }
+        let a = $('.fl_tb').eq(item).html()
+        let new22 = cheerio.load(a, {
             xmlMode: true,
             ignoreWhitespace: true,
             lowerCaseTags: true
         })
-        _arr.push(_a1)
-    })
 
-    for (let i = 0; i < _arr.length; i++) {
-        // 打开新标签,获取最后评论时间和内容
-        if (_arr[i]('.fl_by a').attr('href')) {
-            let page2 = await browser.newPage();
-            await page2.setDefaultNavigationTimeout(0); // 不限制灯带时间
-            await page2.goto('https://www.cmfish.com/bbs/' + _arr[i]('.fl_by a').attr('href'));
-            let content2 = await page2.content();
+        let _arr = []
+        new22('tr').each(async function (i, elem) {
+            let _a1 = cheerio.load($(this).html(), {
+                xmlMode: true,
+                ignoreWhitespace: true,
+                lowerCaseTags: true
+            })
+            _arr.push(_a1)
+        })
 
-            let $2 = cheerio.load(content2, {
-                xmlMode: false,
-                ignoreWhitespace: false,
-                lowerCaseTags: false
-            });
-            let allLength = $2("#postlist").children().length
-            let html2 = $2("#postlist").children().eq(allLength - 2).html()
-            let content2_1 = await cheerio.load(html2);
-            content2_1(".t_f").text()
-            content2_1(".t_f .quote").remove()
-            let lastContent = content2_1(".t_f").text()
-            let time = content2_1(".pti .authi em span").attr("title")
-            let data = {
-                time: time || 0,
-                href: 'https://www.cmfish.com/bbs/' + _arr[i]('.fl_by a').attr('href'),
-                title: lastContent,
-                type: type
+        for (let i = 0; i < _arr.length; i++) {
+            // 打开新标签,获取最后评论时间和内容
+            if (_arr[i]('.fl_by a').attr('href')) {
+                let page2 = await browser.newPage();
+                await page2.setDefaultNavigationTimeout(0); // 不限制灯带时间
+                await page2.goto('https://www.cmfish.com/bbs/' + _arr[i]('.fl_by a').attr('href'));
+                let content2 = await page2.content();
+
+                let $2 = cheerio.load(content2, {
+                    xmlMode: false,
+                    ignoreWhitespace: false,
+                    lowerCaseTags: false
+                });
+                let allLength = $2("#postlist").children().length
+                let html2 = $2("#postlist").children().eq(allLength - 2).html()
+                let content2_1 = await cheerio.load(html2);
+                content2_1(".t_f").text()
+                content2_1(".t_f .quote").remove()
+                let lastContent = content2_1(".t_f").text()
+                let time = content2_1(".pti .authi em span").attr("title")
+                let data = {
+                    time: time || 0,
+                    href: 'https://www.cmfish.com/bbs/' + _arr[i]('.fl_by a').attr('href'),
+                    title: lastContent,
+                    type: type
+                }
+                styduArr.push(data)
+                console.log(data, "data")
             }
-            styduArr.push(data)
-            // console.log(data, "data")
-        }
 
+        }
     }
+
 
     return styduArr
 }
